8712bdfe26557b49b8750e1931647311aad5c369
Tobin717 <raozhongtao@iclou,com>
2021-11-10 20:52:14 +0800

    flask

--- server/test.py
+++ server/test.py
@@ -5,3 +5,9 @@ app=Flask(__name__)
 CORS(app,supports_credentials=True)
 
 
+@app.route('/api/user/extend/updatePersonalHealthData',methods=['POST'])
+def updatePersonalHealthData():
+	if request.headers['Content-Type']=="multipart/form-data":
+		UserExtend=request.form['UserExtend']
+		User=request.form['User']
+		HealthData=request.form['HealthData']

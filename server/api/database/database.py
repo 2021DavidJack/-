0a22e423afc368ab3657d923cddc30bc4aa97f08
Tobin717 <raozhongtao@iclou,com>
2021-11-14 23:21:23 +0800

    datatbase fix

--- server/api/database/database.py
+++ server/api/database/database.py
@@ -1,6 +1,6 @@
 from config import db
 import pymysql
-
+import json
 conn = pymysql.connect(host=db['host'], user=db['user'], passwd=db['passwd'],
                        database=db['database'],
                        charset='utf8',
@@ -10,26 +10,55 @@ db = conn.cursor()
 
 
 def updateSleepData(UserExtend, User, SleepData):
-    return True
+    uid = User['uid']
+    sleepdata1 = SleepData['healthdata1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('UPDATE SleepData SET sleepdata1=%s WHERE uid=%s', (sleepdata1, uid))
+    return db.rowcount
 
 
 def updatePersonalHealthData(UserExtend, User, HealthData):
-    return True
+    uid = User['uid']
+    healthdata1 = HealthData['healthdata1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('UPDATE HealthData SET healthdata1=%s WHERE uid=%s', (healthdata1, uid))
+    return db.rowcount
 
 
 def deletePersonalHealthData(UserExtend, User, HealthData):
-    return True
+    uid = User['uid']
+    healthdata1 = HealthData['healthdata1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('DELETE FROM HealthData  WHERE uid=%s', (uid,))
+    return db.rowcount
 
 
 def deleteSleepData(UserExtend, User, SleepData):
-    return True
+    uid = User['uid']
+    sleepdata1 = SleepData['healthdata1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('DELETE FROM SleepData  WHERE uid=%s', (uid,))
+    return db.rowcount
 
 
 def timeSensitiveReminder(UserExtend, User, Reminder):
-    return True
+    uid = User['uid']
+    remainder1 = Reminder['remainder1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('INSERT INTO Reminder (healthdata1,) VALUES(%s,%s)', (remainder1, uid))
+    return db.rowcount
+
+
+def clearTimeSensitiveReminder(UserExtend, User, Reminder):
+    uid = User['uid']
+    ramaind1 = Reminder['ramaind1']
+    userextend1 = UserExtend['userextend1']
+    db.execute('DELETE FROM Reminder  WHERE uid=%s', (uid,))
+    return db.rowcount
 
 
 def collectSleepData(UserExtend, User, SleepData):
+
     return True
 
 
@@ -42,10 +71,6 @@ def addPersonalHealthData(UserExtend, User, HealthData):
 
 
 def getPersonalHealthData(UserExtend, User, HealthDataCondition):
-    return True
-
-
-def clearTimeSensitiveReminder(UserExtend, User, Reminder):
     return True
 
 
